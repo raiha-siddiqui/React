26c4634625c88351f3030202ec9b0e3e432a8d09
raiha-siddiqui <azrafarooq963@gmail.com>
2023-11-13 09:31:40 +0500

    Episode-02

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom/client';
+
+const parent = React.createElement('div', { id: 'parent' },
+  React.createElement('div', { id: 'child' },
+    [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h2', {}, 'I am h2 tag')]
+  )
+);
+
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello World from React');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering both parent and heading elements
+root.render([parent, heading]);
+
+
